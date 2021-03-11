2072e3e743f1fa343c5aff8b97b88def38cc5a2e
Francis Zinsli <zinsli.alec@gmail.com>
2021-03-10 21:37:01 -0800

    Add project files.

---
+++ ILGPUView/CodeManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ILGPU;
+using ILGPU.Runtime;
+using ILGPU.Runtime.CPU;
+using ILGPU.Runtime.OpenCL;
+using ILGPU.Runtime.Cuda;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+
+namespace ILGPUView
+{
+    public enum AcceleratorType
+    {
+        Default,
+        CPU,
+        Cuda,
+        OpenCL
+    }
+
+    public class CodeManager
+    {
+        public Context context;
+        public Accelerator accelerator;
+
+        public MemoryStream compiledCode;
+        public setupDelegate setup;
+        public loopDelegate loop;
+
+        public CodeManager()
+        {
+            InitializeILGPU(AcceleratorType.CPU);
+            CompileCode(Templates.codeTemplate);
+
+            //compiled at built time
+            ILGPUViewTest.Test.setup(context, accelerator, 100, 100);
+
+            //compiled at run time
+            setup(context, accelerator, 100, 100);
+        }
+
+        private bool InitializeILGPU(AcceleratorType type)
+        {
+            context = new Context(ContextFlags.EnableDebugSymbols | ContextFlags.EnableKernelDebugInformation);
+
+            switch (type)
+            {
+                case AcceleratorType.Default:
+                    accelerator = new CPUAccelerator(context);
+                    break;
+                case AcceleratorType.CPU:
+                    accelerator = new CPUAccelerator(context);
+                    break;
+                case AcceleratorType.Cuda:
+                    accelerator = new CudaAccelerator(context);
+                    break;
+                case AcceleratorType.OpenCL:
+                    accelerator = new CLAccelerator(context, CLAccelerator.AllCLAccelerators.FirstOrDefault());
+                    break;
+            }
+
+            return true;
+        }
+
+        private void CompileCode(string s)
+        {
+            // define source code, then parse it (to the type used for compilation)
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(s);
+
+            // define other necessary objects for compilation
+            string assemblyName = Path.GetRandomFileName();
+            MetadataReference[] references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Context).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Trace).Assembly.Location),
+                MetadataReference.CreateFromFile(Assembly.Load("netstandard, Version=2.0.0.0").Location),
+                MetadataReference.CreateFromFile(Assembly.Load("System.Runtime, Version=5.0.0.0").Location),
+            };
+
+            // analyse and generate IL code from syntax tree
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            compiledCode = new MemoryStream();
+            EmitResult result = compilation.Emit(compiledCode);
+
+            if (!result.Success)
+            {
+                // handle exceptions
+                IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                foreach (Diagnostic diagnostic in failures)
+                {
+                    Trace.WriteLine(diagnostic.Id + ": " + diagnostic.GetMessage());
+                }
+            }
+            else
+            {
+                // load this 'virtual' DLL so that we can use
+                compiledCode.Seek(0, SeekOrigin.Begin);
+                Assembly assembly = Assembly.Load(compiledCode.ToArray());
+
+                // create instance of the desired class and call the desired function
+                Type type = assembly.GetType("ILGPUViewTest.Test");
+                //Object obj = Activator.CreateInstance(type);
+
+                setup = (setupDelegate)Delegate.CreateDelegate(typeof(setupDelegate), type.GetMethod("setup"));
+                loop = (loopDelegate)Delegate.CreateDelegate(typeof(loopDelegate), type.GetMethod("loop"));
+            }
+
+        }
+    }
+}
---
+++ ILGPUView/Templates.cs
@@ -0,0 +1,207 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILGPUView
+{
+    public delegate int setupDelegate(Context context, Accelerator accelerator, int width, int height);
+    public delegate int loopDelegate(ref byte[] bitmap);
+
+    public class Templates
+    {
+
+        public static readonly string usingStatements = @"
+
+                using System;
+                using System.Diagnostics;
+                using ILGPU;
+                using ILGPU.Runtime;
+                using ILGPU.Runtime.CPU;
+
+";
+        public static readonly string codeTemplate = usingStatements + @"
+
+
+namespace ILGPUViewTest
+{
+    public struct Color
+    {
+        public float r;
+        public float g;
+        public float b;
+
+        public Color(float r, float g, float b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    public struct Canvas
+    {
+        public ArrayView2D<Color> canvas;
+        public int sizeX;
+        public int sizeY;
+
+        public Canvas(ArrayView2D<Color> canvas, int sizeX, int sizeY)
+        {
+            this.canvas = canvas;
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+        }
+
+        public void setColor(Index2 index, Color c)
+        {
+            canvas[index] = c;
+        }
+    }
+
+    public static class Test
+    {
+        static Action<Index2, Canvas, ArrayView<byte>> outputKernel;
+        static Action<Index2, Canvas> userKernel;
+
+        static Canvas c;
+        static MemoryBuffer2D<Color> canvasData;
+        static MemoryBuffer<byte> bitmapData;
+
+        public static void CanvasToBitmap(Index2 index, Canvas c, ArrayView<byte> bitmap)
+        {
+            int newIndex = ((index.Y * c.sizeY) + index.X) * 3;
+            Color color = c.canvas[index];
+
+            bitmap[newIndex] = (byte)(255.99f * color.r);
+            bitmap[newIndex + 1] = (byte)(255.99f * color.g);
+            bitmap[newIndex + 2] = (byte)(255.99f * color.b);
+        }
+
+        public static int setup(Context context, Accelerator accelerator, int width, int height)
+        {
+            canvasData = accelerator.Allocate<Color>(height, width);
+            c = new Canvas(canvasData, height, width);
+
+            bitmapData = accelerator.Allocate<byte>(width * height * 3);
+
+            outputKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, Canvas, ArrayView<byte>>(CanvasToBitmap);
+            userKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, Canvas>(kernel);
+
+            return 1;
+        }
+
+        public static int loop(ref byte[] bitmap)
+        {
+            userKernel(canvasData.Extent, c);
+            outputKernel(canvasData.Extent, c, bitmapData);
+            bitmapData.CopyTo(bitmap, 0, 0, bitmap.Length);
+            return 1;
+        }
+
+        public static void dispose()
+        {
+            canvasData.Dispose();
+            bitmapData.Dispose();
+        }
+
+        public static void kernel(Index2 index, Canvas c)
+        {
+            c.setColor(index, new Color(index.X / c.sizeX, index.Y / c.sizeY, 0));
+        }
+    }
+}
+";
+    }
+}
+
+
+namespace ILGPUViewTest
+{
+    public struct Color
+    {
+        public float r;
+        public float g;
+        public float b;
+
+        public Color(float r, float g, float b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    public struct Canvas
+    {
+        public ArrayView2D<Color> canvas;
+        public int sizeX;
+        public int sizeY;
+
+        public Canvas(ArrayView2D<Color> canvas, int sizeX, int sizeY)
+        {
+            this.canvas = canvas;
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+        }
+
+        public void setColor(Index2 index, Color c)
+        {
+            canvas[index] = c;
+        }
+    }
+
+    public static class Test
+    {
+        static Action<Index2, Canvas, ArrayView<byte>> outputKernel;
+        static Action<Index2, Canvas> userKernel;
+
+        static Canvas c;
+        static MemoryBuffer2D<Color> canvasData;
+        static MemoryBuffer<byte> bitmapData;
+
+        public static void CanvasToBitmap(Index2 index, Canvas c, ArrayView<byte> bitmap)
+        {
+            int newIndex = ((index.Y * c.sizeY) + index.X) * 3;
+            Color color = c.canvas[index];
+
+            bitmap[newIndex] = (byte)(255.99f * color.r);
+            bitmap[newIndex + 1] = (byte)(255.99f * color.g);
+            bitmap[newIndex + 2] = (byte)(255.99f * color.b);
+        }
+
+        public static int setup(Context context, Accelerator accelerator, int width, int height)
+        {
+            canvasData = accelerator.Allocate<Color>(height, width);
+            c = new Canvas(canvasData, height, width);
+
+            bitmapData = accelerator.Allocate<byte>(width * height * 3);
+
+            outputKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, Canvas, ArrayView<byte>>(CanvasToBitmap);
+            userKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, Canvas>(kernel);
+
+            return 1;
+        }
+
+        public static int loop(ref byte[] bitmap)
+        {
+            userKernel(canvasData.Extent, c);
+            outputKernel(canvasData.Extent, c, bitmapData);
+            bitmapData.CopyTo(bitmap, 0, 0, bitmap.Length);
+            return 1;
+        }
+
+        public static void dispose()
+        {
+            canvasData.Dispose();
+            bitmapData.Dispose();
+        }
+
+        public static void kernel(Index2 index, Canvas c)
+        {
+            c.setColor(index, new Color(index.X / c.sizeX, index.Y / c.sizeY, 0));
+        }
+    }
+}
